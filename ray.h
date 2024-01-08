e1011b965626a1f65c3bf85fd5cccf2974eb3f45
Marcus Chong <marcus.jn.chong@gmail.com>
2024-01-08 12:59:27 -0500

    ray calss

---
+++ ray.h
@@ -0,0 +1,23 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+    public:
+        ray() {} // empty constructor
+        ray(const point3& origin, const vec3& direction): orig(origin), dir(direction) {} // member initializer list
+
+        point3 origin() const {
+            return orig;
+        }
+
+        vec3 at(double t) const {
+            return orig + dir*t;
+        }
+    private:
+        point3 orig;
+        vec3 dir;
+};
+
+#endif
