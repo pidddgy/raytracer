bfb174464d914c4685f7c84cd56e2d9479615884
Marcus Chong <marcus.jn.chong@gmail.com>
2024-01-11 17:32:00 -0500

    render spheres

--- main2.cpp
+++ main2.cpp
@@ -4,7 +4,24 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, const double radius,const ray& r) {
+    auto oc = r.origin()-center;
+    auto a = dot_product(r.direction(), r.direction());
+    auto b = 2*dot_product(r.direction(), oc);
+    auto c = dot_product(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant>=0);
+}
+
 color ray_color(const ray& r) {
+    if(hit_sphere(point3(-0.5, 0, -1), 0.2, r)) {
+        return color(1, 0, 0);
+    }
+    if(hit_sphere(point3(0.5, 0, -1), 0.2, r)) {
+        return color(1, 0, 0);
+    }
+    
+    
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return color(1.0, 1.0, 1.0)*(1.0-a) + color(0.5, 0.7, 1.0)*a;
